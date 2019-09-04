0d0ba8b0cd0882f4a7938574c85138115303a228
Juan Pablo Armenta <juanpabload98@gmail.com>
2019-09-03 23:21:20 -0500

    se ordeno mas

--- src/Principal.java
+++ src/Principal.java
@@ -3,61 +3,51 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Principal {
     public static void main(String[] args )throws Exception{
-        ServerSocket sockeServer =new ServerSocket(2500);
-        Socket socket = sockeServer.accept();
-        BufferedReader lector =new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String entrada;
-        Scanner scanner =new  Scanner(System.in);
-        String salida;
-        PrintWriter escritor= new PrintWriter(socket.getOutputStream(),true);
-        do {            
-            entrada=lector.readLine();
-            
-            System.out.println(entrada);
-            if (entrada.equalsIgnoreCase("fin")) {
-                System.out.println("me voy");
-                socket.close();
-                sockeServer.close();
-                System.exit(0);
-            }
-            salida=scanner.nextLine();
-            escritor.println(salida);
-        } while (!entrada.equalsIgnoreCase("fin"));
+       CrearServidor();
     }
     
     public static void CrearServidor(){
         try {
          ServerSocket sockeServer =new ServerSocket(2500);
         Socket socket = sockeServer.accept();
+         ConfigurarServidor(socket, sockeServer);
         } catch (Exception e) {
            System.out.println("Error al crear socket del servidor "+e);
         }     
     }
-    public static void ConfigurarServidor(Socket socket, ServerSocket sockeServer){
+    public static void ConfigurarServidor(Socket socket, ServerSocket sockeServer) {
+        BufferedReader lector;
         try {
-            
-        } catch (Exception e) {
-        }
-        BufferedReader lector =new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String entrada;
-        Scanner scanner =new  Scanner(System.in);
-        String salida;
-        PrintWriter escritor= new PrintWriter(socket.getOutputStream(),true);
-        do {            
-            entrada=lector.readLine();
-            
-            System.out.println(entrada);
-            if (entrada.equalsIgnoreCase("fin")) {
-                cerrarSockets(socket, sockeServer);
+            lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String entrada;
+            Scanner scanner = new Scanner(System.in);
+            String salida;
+            PrintWriter escritor;
+            try {
+                escritor = new PrintWriter(socket.getOutputStream(), true);
+                do {
+                    entrada = lector.readLine();
+
+                    System.out.println(entrada);
+                    if (entrada.equalsIgnoreCase("fin")) {
+                        cerrarSockets(socket, sockeServer);
+                    }
+                    salida = scanner.nextLine();
+                    escritor.println(salida);
+                } while (!entrada.equalsIgnoreCase("fin"));
+            } catch (IOException ex) {
+                System.out.println("Erro al crear escritor" + ex);
             }
-            salida=scanner.nextLine();
-            escritor.println(salida);
-        } while (!entrada.equalsIgnoreCase("fin"));
-        
+        } catch (IOException ex) {
+            System.out.println("Error al crear lector" + ex);
+        }
+
     }
     public static void cerrarSockets(Socket socket, ServerSocket sockeServer){
         try {
